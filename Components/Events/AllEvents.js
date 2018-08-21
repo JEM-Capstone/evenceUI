9d6343adc8bf2a2bc5efbf2a3d2d47362b570e83
Jehoshuah K <musicalbard@gmail.com>
2018-08-21 11:58:26 -0500

    single oath working and storing user's data

--- Components/Events/AllEvents.js
+++ Components/Events/AllEvents.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { FlatList, ListItem, List, Text, View, Button, Image } from 'react-native';
+import {
+  FlatList,
+  ListItem,
+  List,
+  Text,
+  View,
+  Button,
+  Image,
+} from 'react-native';
 import { Query, Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
 import { SafeAreaView } from 'react-navigation';
 import styles from '../styles';
-import Favoriting from './Favoriting'
+import Favoriting from './Favoriting';
 
 //userId will need to be a variable, depending on who is logged in
 const allEventsQuery = gql`
   {
-    events (userId: 1) {
+    events(userId: 1) {
       id
       eventName
       eventGroup
@@ -26,33 +34,61 @@ const allEventsQuery = gql`
 //Note to evelyn: you need to sort the data array (by date!) b4 render so it doesn't keep reordering
 class AllEvents extends Component {
   render() {
-    const { navigation: { navigate, push } } = this.props;
+    const {
+      navigation: { navigate, push },
+    } = this.props;
     return (
-        <SafeAreaView style={styles.container}>
-          <View>
-            <Query query={allEventsQuery}>
-              {({ data: { events }, error, loading }) => {
-                if (error) return (<View><Text>There was an error</Text></View>)
-                if (loading) return (<View><Text>Loading the data</Text></View>)
-                return (<FlatList
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Query query={allEventsQuery}>
+            {({ data: { events }, error, loading }) => {
+              if (error)
+                return (
+                  <View>
+                    <Text>There was an error</Text>
+                  </View>
+                );
+              if (loading)
+                return (
+                  <View>
+                    <Text>Loading the data</Text>
+                  </View>
+                );
+              return (
+                <FlatList
                   data={events}
                   keyExtractor={(item, index) => item.id}
                   renderItem={({ item }) => (
                     <View>
                       <View>
                         <Image
-                          source={item.photo ? { uri: item.photo } : require(`../../resources/calendar.png`)}
-                          style={item.photo ? { flex: 1, height: 200 } : { flex: 1, height: 200, alignSelf: `center`}  }
+                          source={
+                            item.photo
+                              ? { uri: item.photo }
+                              : require(`../../resources/calendar.png`)
+                          }
+                          style={
+                            item.photo
+                              ? { flex: 1, height: 200 }
+                              : { flex: 1, height: 200, alignSelf: `center` }
+                          }
                           resizeMode="contain"
                         />
 
-                        <Favoriting eventId={item.id} favorite={item.favorite}/>
-
+                        <Favoriting
+                          eventId={item.id}
+                          favorite={item.favorite}
+                        />
                       </View>
                       <View style={styles.listText}>
-
-                        <Text style={styles.header} onPress={() => push(`SingleEvent`, {eventId: item.id} )}>
-                          {`Event: ${item.eventName}`}</Text>
+                        <Text
+                          style={styles.header}
+                          onPress={() =>
+                            push(`SingleEvent`, { eventId: item.id })
+                          }
+                        >
+                          {`Event: ${item.eventName}`}
+                        </Text>
 
                         <Text>{`Group: ${item.eventGroup}`}</Text>
                         <Text>{`Date: ${item.date} | Time: ${item.time}`}</Text>
@@ -62,11 +98,12 @@ class AllEvents extends Component {
                       </View>
                     </View>
                   )}
-                />)
-              }}
-            </Query>
-          </View>
-        </SafeAreaView>
+                />
+              );
+            }}
+          </Query>
+        </View>
+      </SafeAreaView>
     );
   }
 }
--- Components/Login.js
+++ Components/Login.js
@@ -1,64 +1,68 @@
-import React from 'react'
-import { Button, StyleSheet, Text, View, Linking } from 'react-native'
-import { AuthSession, WebBrowser } from 'expo'
-import axios from 'axios'
-
+import React from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  AsyncStorage,
+} from 'react-native';
+import { AuthSession, WebBrowser } from 'expo';
+import axios from 'axios';
 
 export default class Login extends React.Component {
   state = {
-    serverUrl: `http://172.17.20.3:8080`, // this should change depending on where the server is running
+    serverUrl: `http://172.17.21.36:8080`, // this should change depending on where the server is running
     result: null,
     accessToken: '',
     linkedinBasicProfile: '',
   };
 
   render() {
-    const { navigate } = this.props.navigation
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Button title="| Login with LinkedIn |" onPress={this.handleOAuthLogin} />
-        <Button title="| Test |" onPress={this.test} />
-        {this.state.result ? ( <Text>{JSON.stringify(this.state.result.type)}</Text> ) : null}
+        <Button
+          title="| Login with LinkedIn |"
+          onPress={this.handleOAuthLogin}
+        />
+        {this.state.result ? (
+          <Text>{JSON.stringify(this.state.result.type)}</Text>
+        ) : null}
       </View>
-    )
-  }
-
-  test = async () => {
-    console.log('test!')
-    try {
-      let clientId = await axios.get(`${this.state.serverUrl}/auth/linkedin/clientid`)
-      let clientSecret = await axios.get(`${this.state.serverUrl}/auth/linkedin/clientsecret`)
-      let appState = await axios.get(`${this.state.serverUrl}/auth/linkedin/appstate`)
-    } catch (err) {
-      console.log('An error',err);
-      alert(err.message)
-    }
+    );
   }
 
   handleOAuthLogin = async () => {
     // gets the url to direct back to the app after any request to linkedin
-    let redirectUrl = AuthSession.getRedirectUrl()
+    let redirectUrl = AuthSession.getRedirectUrl();
+    console.log(redirectUrl);
 
+    // let clientId = await axios.get(
+    //   `${this.state.serverUrl}/auth/linkedin/clientid`
+    // );
+    // let clientSecret = await axios.get(
+    //   `${this.state.serverUrl}/auth/linkedin/clientsecret`
+    // );
+    // let appState = await axios.get(
+    //   `${this.state.serverUrl}/auth/linkedin/appstate`
+    // );
 
-    let clientId = await axios.get(`${this.state.serverUrl}/auth/linkedin/clientid`)
-    let clientSecret = await axios.get(`${this.state.serverUrl}/auth/linkedin/clientsecret`)
-    let appState = await axios.get(`${this.state.serverUrl}/auth/linkedin/appstate`)
+    // await this.getAuthCode(redirectUrl, clientId.data, appState.data);
 
-    await this.getAuthCode(redirectUrl, clientId.data, appState.data)
+    // // this check gaurds against CSRF attacks
+    // if (this.state.result.params.state !== appState.data) {
+    //   console.log('Not Authorized!'); // this should be a more useful message and also throw a HTTP 401 error
+    // } else {
+    //   await this.getAccessToken(redirectUrl, clientId.data, clientSecret.data);
+    // }
 
-    // this check gaurds against CSRF attacks
-    if (this.state.result.params.state !== appState.data) {
-      console.log('Not Authorized!') // this should be a more useful message and also throw a HTTP 401 error
-    } else {
-      await this.getAccessToken(redirectUrl, clientId.data, clientSecret.data)
-    }
-
-    // Get that basic profile info including linkedinId
-    await this.getBasicUserProfile(this.state.accessToken)
+    // // Get that basic profile info including linkedinId
+    // await this.getBasicUserProfile(this.state.accessToken);
     // Get more complete info on our backend/database
-    let backendAuth = await this.handlePassportLogin()
-    console.log('BACKEND AUTH:', backendAuth)
-  }
+    let backendAuth = await this.handlePassportLogin();
+    console.log('BACKEND AUTH:', backendAuth);
+  };
 
   // Get an auth code for LinkedIn
   getAuthCode = async (redirectUrl, clientId, appState) => {
@@ -71,54 +75,54 @@ export default class Login extends React.Component {
         `&client_id=${clientId}` +
         `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
         `&state=${appState}`,
-        // `&scope=r_basicprofile`,
-    })
+      // `&scope=r_basicprofile`,
+    });
     // save the data we get back, including the auth code
-    await this.setState({ result })
+    await this.setState({ result });
     // console.log('access code:', this.state.result)
-  }
+  };
 
   // Get an access Token from linkedin
   getAccessToken = async (redirectUrl, clientId, clientSecret) => {
-
     try {
       let getTokenUrl =
-      `https://www.linkedin.com/oauth/v2/accessToken` +
-      `?grant_type=authorization_code` +
-      `&code=${this.state.result.params.code}` +
-      `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
-      `&client_id=${clientId}` +
-      `&client_secret=${clientSecret}`
+        `https://www.linkedin.com/oauth/v2/accessToken` +
+        `?grant_type=authorization_code` +
+        `&code=${this.state.result.params.code}` +
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+        `&client_id=${clientId}` +
+        `&client_secret=${clientSecret}`;
 
-      let res = await axios.post(getTokenUrl)
+      let res = await axios.post(getTokenUrl);
       // console.log('Access Token (res.data):', res.data)
-      await this.setState({ accessToken: res.data.access_token })
+      await this.setState({ accessToken: res.data.access_token });
       // console.log('Access Token:', this.state.accessToken)
     } catch (err) {
-      console.log('ERROR WHILE GETTING TOKEN:', err)
+      console.log('ERROR WHILE GETTING TOKEN:', err);
     }
-  }
-
+  };
 
   // Get the v1 api user profile info --> we're most interested in the linkedinId for more complete user info lookup later
-  getBasicUserProfile = async (accessToken) => {
+  getBasicUserProfile = async accessToken => {
     try {
       // Get the user info
-      let {data} = await axios.get(`https://api.linkedin.com/v1/people/~?oauth2_access_token=${accessToken}&format=json`)
-      this.setState({ linkedinBasicProfile: data })
-      console.log('the user data for the frontend:', data)
-    } catch(err) {
-      console.log('Error getting user profile:', err)
+      let { data } = await axios.get(
+        `https://api.linkedin.com/v1/people/~?oauth2_access_token=${accessToken}&format=json`
+      );
+      this.setState({ linkedinBasicProfile: data });
+      console.log('the user data for the frontend:', data);
+    } catch (err) {
+      console.log('Error getting user profile:', err);
     }
-  }
+  };
 
-  handleRedirect = async (event) => {
+  handleRedirect = async event => {
     // WebBrowser.dismissBrowser();
     // WebBrowser.dismissAuthSession();
     const data = Linking.parse(event.url);
     // await this.setState({ redirectData: data })
     console.log(`data in handleRedirect`, data);
-  }
+  };
 
   // Login...AGAIN --> This time using passport & all on the backend so that we can save the more complete user info made available when passport is used
   handlePassportLogin = async () => {
@@ -128,8 +132,22 @@ export default class Login extends React.Component {
     const redirectUrl = await Linking.getInitialURL();
 
     try {
-      const authResult = await WebBrowser.openAuthSessionAsync(`${this.state.serverUrl}/auth/linkedin/`, redirectUrl);
-      await this.setState({ authResult });
+      const authResult = await WebBrowser.openAuthSessionAsync(
+        `${this.state.serverUrl}/auth/linkedin/`,
+        redirectUrl
+      );
+      const { data } = await axios.get(
+        `${this.state.serverUrl}/auth/linkedin/user`
+      );
+      console.log('this is front end user info:', data);
+      _storeData = async () => {
+        try {
+          await AsyncStorage.setItem(`userId`, data.linkedinId);
+        } catch (error) {
+          // Error saving data
+        }
+      };
+      // await this.setState({ authResult });
       // Below ostensibly returns a user profile - but it currently isn't
       // let userAttempt = await axios.get(`http://172.17.20.3:8080/auth/linkedin/me`)
       // console.log('userAttempt', userAttempt)
@@ -138,20 +156,20 @@ export default class Login extends React.Component {
     }
 
     this.removeLinkingListener();
-  }
+  };
 
   addLinkingListener = () => {
     Linking.addEventListener(`url`, this.handleRedirect);
-  }
+  };
 
   removeLinkingListener = () => {
     Linking.removeEventListener(`url`, this.handleRedirect);
-  }
+  };
 
   handleLogout = async () => {
-    let res = await axios.get(`${this.state.authUrl}/auth/linkedin/logout`)
-    await this.setState({ authResult: {} })
-  }
+    let res = await axios.get(`${this.state.authUrl}/auth/linkedin/logout`);
+    await this.setState({ authResult: {} });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -162,8 +180,6 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
 /* ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     YOU CAN USE THE FOLLOWING ROUTES TO TEST WHAT YOU GET BACK FROM LINKED IN
  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
@@ -173,6 +189,5 @@ const styles = StyleSheet.create({
 // 'https://api.linkedin.com/v1/people/~?oauth2_access_token=AQV3irm1Xgl5JVkOW2x5jXXA-9euAXvrvNFKvx8q8pDLjtW9nSWysolo9jI6jG7dRiPejmbUt8o1kj7OFNwGbfuAChnHM7tlRXweMM-8lm3_TaCGZ_DKgUUOEANrocIp6lkHQjklaWo-fVtSkzOj95553DH96p5KrG268Zax-Gpl7EtDazuenvu-r90JAgV2RRhAv68d-g1qCczGz1xFihbqUBoAAVJckoT0wmF7eoeiL2zUHGx6cQ2gbHJFQqNuM1RGDBtW-JUubcYtFq0735-106dAH1ilKrc8B6sX8IpResSEnYgjo2OSXYhRkXV0EzN79lna6qn584J_6-gf6gCv3Cnh5w&format=json'
 // 'https://api.linkedin.com/v2/me/~?oauth2_access_token=AQVK8JM9VnAh_5NR1vE2YAAVNHJDX7-JTuEescS8OFruQ8TShx-SMKBy-NcK7_WCbYSCuI6z-_DAaPv8HhuJoYtccFmot3qr1ohoGBSSlYq1cZeot1pDpePM5wLsbiDZkaFH-fkLRMngp3Kbsid-yF3djv0lPGFddZFhcG4K31Z7uF58izsPeLUCCZ3miAMtyPKDF1ljoLRX_O_HileoZ3hA-MziplbgCg994rXDPY_qYHwAgdjgkemMMn_0ut9PpYRMsNSewbxhZKt8x7MXtJCVXUqA5m_y2qWkHfcyoHNrvLl3ui24IL5aSbvMH2rBn7dL1Ru5KPyMG-pDB8LT9ZeESqLPfA'
 // https://api.linkedin.com/v2/me?format=json
-
 
 // `https://www.linkedin.com/uas/oauth2/authorization?client_id=759dczzx23nyic&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Flinkedin%2Fcallback&response_type=code&scope=r_basicprofile+r_emailaddress&state=8da572e31a8e66e6b1de54acddd14937d976ed06d7ed3217&client_id=*`
